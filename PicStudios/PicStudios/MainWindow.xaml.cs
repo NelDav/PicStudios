b718e8df19b1296f990d32cba9d16d207114bf5e
David Nelles <easyprogramms@gmail.com>
2018-07-20 23:33:58 +0200

    Added Toolbox Added Propertybox

---
+++ PicStudios/PicStudios/MainWindow.xaml.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace PicStudios
+{
+    /// <summary>
+    /// Interaktionslogik für MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+
+
+        #region Resize Toolbar
+        bool toolResize = false;
+        private void grd_Tool_Size_MouseDown(object sender, MouseEventArgs e)
+        {
+            toolResize = true;
+        }
+
+        private void grd_Tool_Size_MouseUp(object sender, MouseEventArgs e)
+        {
+            toolResize = false;
+        }
+
+        private void grd_Tool_Size_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (toolResize)
+                MainGrid.RowDefinitions.ElementAt(0).Height = new GridLength(e.GetPosition(MainGrid).Y + 2);
+        }
+        #endregion
+
+        #region Resize property
+        bool propertyResize = false;
+        private void grd_Property_Size_MouseDown(object sender, MouseEventArgs e)
+        {
+            propertyResize = true;
+        }
+
+        private void grd_Property_Size_MouseUp(object sender, MouseEventArgs e)
+        {
+            propertyResize = false;
+        }
+
+        private void grd_Property_Size_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (propertyResize)
+                InletGrid.ColumnDefinitions.ElementAt(1).Width = new GridLength(InletGrid.ActualWidth - e.GetPosition(InletGrid).X + 2);
+        }
+        #endregion
+    }
+}
